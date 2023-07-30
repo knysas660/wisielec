d84a6d0bcf27fbdb7855e501eb45bb7412c3e074
kaliluk <kaliluk@kalihost.kaliluk>
2023-07-30 19:31:43 +0200

    moja pierwsza rewizja

---
+++ wisielec.py
@@ -0,0 +1,51 @@
+#python_lst239
+
+def hangman(word):
+    wrong = 0
+    stages = ['',
+              '__________     __________',
+              '|                        ',
+              '|           |            ',
+              '|           O            ',
+              '|          /|\           ',
+              '|          / \           ',
+              '|                        '
+              ]
+    rletters = list(word)
+    board = ['_'] * len(word)
+    win = False
+    print('Gra w wisielca')
+    while wrong < len(stages) -1:
+        print('\n')
+        msg = 'Odgadnij literę: '
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((' '.join(board)))
+        e = wrong + 1
+        print('\n'.join(stages[0:e]))
+        if '_' not in board:
+            print('Wygrałeś!')
+            print(' '.join(board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0: wrong]))
+        print('Przegrałeś! Miałeś odgadnąć: {}.'.format(word))
+        
+x=['kot','pies','człowiek','mutant','osa']
+
+y=len(x)-1
+
+import random
+
+i=random.randint(0,y)
+
+
+
+hangman(x[i])
+
